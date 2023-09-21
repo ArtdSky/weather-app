5f2b40dcdca24218246715f8c7ba75d4cdf38712
Artem <adpm@list.ru>
2023-09-21 18:59:56 +0600

    Add cache to database

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Cache;
 
 class WeatherController extends Controller
 {
@@ -16,13 +17,24 @@ class WeatherController extends Controller
 
     public function fetchWeather(Request $request)
     {
-        $city = $request->input('city');
-        $apiKey = env('OPENWEATHERMAP_KEY');
 
-        $client = new Client();
-        $response = $client->get("http://api.openweathermap.org/data/2.5/weather?q={$city}&appid={$apiKey}&units=metric");
-        $data = json_decode($response->getBody(), true);
+        $city = $request->input('city');
+        $data = Cache::get($city);
+        if (!$data) {
+            $apiKey = env('OPENWEATHERMAP_KEY');
+
+            $client = new Client();
+            $response = $client->get("http://api.openweathermap.org/data/2.5/weather?q={$city}&appid={$apiKey}&units=metric");
+
+            $data = json_decode($response->getBody(), true);
+
+            // Сохраняем данные о погоде в кеше на 1 час
+            Cache::put($city, $data, 3600);
+        }
+
         return response()->json($data);
+
+
     }
 
 }
